0761537b8c423dab79ee0e5c20818eb254c16abd
nanakim-star <rorhtn777@gmail.com>
2025-05-25 01:46:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from telegram import Bot
 import asyncio
 import os
 
-TOKEN = os.getenv("7622493232:AAFtDWDvx8giDqSOmuH4K6QB9m5K3ljCzRs")  # Render 환경변수에서 토큰 불러오기
-CHAT_ID = os.getenv("4831865253")  # 메시지를 보낼 그룹방 ID
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=TOKEN)
 
 async def send_periodic_message():
     while True:
         try:
-            await bot.send_message(chat_id=CHAT_ID, text="🎯 Welcome! ZERO ROOM에 오신것을 환영합니다!")
+            await bot.send_message(chat_id=CHAT_ID, text="🎯 Welcome! ZERO ROOM에 오신걸 환영합니다!")
             print("메시지 전송됨")
         except Exception as e:
             print(f"오류 발생: {e}")
